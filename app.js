d8891733ebce7f4a3d781d60bb433aaeba739a8f
Tedros Kahsu <tedkahcom@gmail.com>
2024-02-07 17:07:57 +0300

    use express json

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 4000;
 const peopleRoutes = require("./routes/peopleRoutes");
 
+app.use(express.json());
 app.use("/api", peopleRoutes);
 
 app.listen(port, () => {
